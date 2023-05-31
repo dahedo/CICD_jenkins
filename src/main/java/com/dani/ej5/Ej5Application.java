abdb3f9b7f1ee01b99ff075a951df7f13631dbae
dahedo <dahedo@externos.itnow.com>
2023-05-31 14:10:28 +0200

    Nueva version de codigo

--- src/main/java/com/dani/ej5/Ej5Application.java
+++ src/main/java/com/dani/ej5/Ej5Application.java
@@ -1,4 +1,4 @@
-package com.ej5.ej5;
+package com.dani.ej5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/dani/ej5/Ej5ApplicationTests.java
+++ src/test/java/com/dani/ej5/Ej5ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ej5.ej5;
+package com.dani.ej5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
